181bdfdfe3f46379656ba4da9eabddd25cfaa26d
Nizar <nizar.al.nasraoui@gmail.com>
2023-03-10 00:28:57 +0100

    async pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductModule } from './product/product.module';
+import {HttpClientModule} from '@angular/common/http';
+import { EffectsModule } from "@ngrx/effects";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    ProductModule,
+    BrowserModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
